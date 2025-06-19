5aefbe2fc1c52db62116db4cfbb8d5c89ef37248
BigMacison <leonard.soft1122@gmail.com>
2025-06-19 14:22:10 +0000

    Added code to main.py for API

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from libraries.DownloadHandler import DownloadHandler
+import uvicorn
+import webbrowser
 import asyncio
+import threading
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from libraries.LogHelper import LogHelper
+from libraries.DownloadHandler import DownloadHandler
 
+app = FastAPI()
+logger = LogHelper()
+DownloadHandler().ensure_binaries()
 
-async def main():
-  await DownloadHandler().ensure_binaries()
+@app.get("/")
+async def index():
+  return {"message": "Hello World"} 
 
+def open_browser_later():
+  asyncio.run(_wait_and_open())
 
-if __name__ == '__main__':
-    asyncio.run(main())
+async def _wait_and_open():
+  import aiohttp
+  while True:
+    try:
+      async with aiohttp.ClientSession() as session:
+        async with session.get("http://127.0.0.1:8000/") as resp:
+          if resp.status == 200:
+            await logger.passLog(2, "Uvicorn finished starting")
+            break
+    except:
+      pass
+    await asyncio.sleep(0.2)
+  await logger.passLog(2, "opening browser..")
+  webbrowser.open("http://127.0.0.1:8000/")
+
+if __name__ == "__main__":
+  threading.Thread(target=open_browser_later, daemon=True).start()
+  asyncio.run(logger.passLog(2, "Starting Uvicorn server..."))
+  uvicorn.run(app, host="127.0.0.1", port=8000)
