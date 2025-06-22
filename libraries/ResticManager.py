45ed659cc0a2e5d7ee4ee42ee335fe2c571620b1
BigMacison <leonard.soft1122@gmail.com>
2025-06-22 13:55:58 +0000

    Made ResticManager require no password

--- libraries/ResticManager.py
+++ libraries/ResticManager.py
@@ -6,9 +6,8 @@ from .SubprocessHandler import SubprocessHandler
 from .LogHelper import LogHelper
 
 class ResticManager:
-  def __init__(self, endpoint: str, password: str, keep_hourly: int = 0, keep_daily: int = 0, keep_weekly: int = 0):
+  def __init__(self, endpoint: str, keep_hourly: int = 0, keep_daily: int = 0, keep_weekly: int = 0):
     self.endpoint = endpoint
-    self.password = password
     self.keep_hourly = keep_hourly
     self.keep_daily = keep_daily
     self.keep_weekly = keep_weekly
@@ -17,14 +16,14 @@ class ResticManager:
     self.restic_binary_path = "./bin/restic/restic.exe" if os.name == "nt" else "./bin/restic/restic"
     self.rclone_binary_path = "./bin/rclone/rclone.exe" if os.name == "nt" else "./bin/rclone/rclone"
     self.rclone_config_path = os.getcwd() + "/configs/rclone.conf"
-    self.env = {"RESTIC_PASSWORD": self.password, "RCLONE_CONFIG": self.rclone_config_path}
+    self.env = {"RCLONE_CONFIG": self.rclone_config_path}
     self.logger = LogHelper()
   
   async def backupRepo(self, local_path: str, remote_path: str, callback_function=None):
     # Uploads/backups a certain file/folder (specified as path) into a remote repository (can't be used simultaniously with restoreRepo())
     await self.logger.passLog(2, f"Starting backup from '{local_path}' to '{remote_path}'")
     async with self._lock:
-      self.process = SubprocessHandler([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "backup", local_path], self.env)
+      self.process = SubprocessHandler([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "backup", local_path], self.env)
       if callback_function is not None:
         self.process.register_listener(callback_function)
       self.process.start()
@@ -34,7 +33,7 @@ class ResticManager:
     # Downloads/restores a certain file/folder (specified as path) from a remote repository (can't be used simultaniously with backupRepo())
     await self.logger.passLog(2, f"Starting restore from '{remote_path}' to '{local_path}', snapshot='{snapshot}'")
     async with self._lock:
-      self.process = SubprocessHandler([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "restore", snapshot, "--target", local_path], self.env)
+      self.process = SubprocessHandler([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "restore", snapshot, "--target", local_path], self.env)
       if callback_function is not None:
         self.process.register_listener(callback_function)
       self.process.start()
@@ -45,6 +44,15 @@ class ResticManager:
     await self.logger.passLog(3, "Waiting for process to complete...")
     await self.process.wait_until_done()
     await self.logger.passLog(2, "Process completed.")
+
+  def createRemoteFolder(self, remote_path: str):
+    # Creates a folder on the remote endpoint at the specified path
+    asyncio.create_task(self.logger.passLog(2, f"Creating folder at remote path '{remote_path}'"))
+    try:
+      SubprocessHandler.run_once([self.rclone_binary_path, "mkdir", f"{self.endpoint}:{remote_path}"], self.env)
+      asyncio.create_task(self.logger.passLog(2, f"Successfully created folder at '{remote_path}'"))
+    except Exception as e:
+      asyncio.create_task(self.logger.passLog(0, f"Failed to create remote folder at '{remote_path}': {str(e)}"))
 
   def downloadPath(self, remote_path: str, local_path: str):
     # Downloads remote file/folder that isn't part of a repository.
@@ -69,20 +77,20 @@ class ResticManager:
   def getSnapshots(self, remote_path: str) -> list:
     # Gets all snapshots
     asyncio.create_task(self.logger.passLog(2, f"Getting snapshots from '{remote_path}'"))
-    return json.loads(SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "snapshots"], self.env))
+    return json.loads(SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "snapshots"], self.env))
 
   def initRepo(self, remote_path: str):
     # creates a repository at the specified path
     asyncio.create_task(self.logger.passLog(2, f"Initializing repository at '{remote_path}'"))
-    SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "init"], self.env)
+    SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "init"], self.env)
 
   def removeOldSnapshots(self, remote_path):
     asyncio.create_task(self.logger.passLog(2, f"Removing old snapshots at '{remote_path}'"))
-    SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "forget", "--keep-hourly", self.keep_hourly, "--keep-daily", self.keep_daily, "--keep-weekly", self.keep_weekly, "--prune"], self.env)
+    SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "forget", "--keep-hourly", self.keep_hourly, "--keep-daily", self.keep_daily, "--keep-weekly", self.keep_weekly, "--prune"], self.env)
 
   def isRepo(self, remote_path: str) -> bool:
     try:
-      output_str = SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "snapshots"], self.env)
+      output_str = SubprocessHandler.run_once([self.restic_binary_path, "-r", f"rclone:{self.endpoint}:{remote_path}", "--insecure-no-password", "--option", f"rclone.program={self.rclone_binary_path}", "--json", "snapshots"], self.env)
       output_json = json.loads(output_str)
       is_repo = output_json["code"] != 10
       asyncio.create_task(self.logger.passLog(2, f"Checked repo at '{remote_path}': Exists = {is_repo}"))
